83632405de136f3fbc9cab3b463572d96fed5ed5
gopularamarchitha-ui <gopularamarchitha@gmail.com>
2026-08-12 16:31:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ Format the output neatly.
 
     try:
         response = client.models.generate_content(
-            model="gemini-2.5-flash",
+            model="gemini-3.5-flash",
             contents=prompt
         )
 
